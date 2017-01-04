f35bbeba8ebf690420bd8a68916c2a23345cc415
PrintBug <bug@mrfred.xyz>
2017-01-03 20:57:54 -0500

    Fixes. and name changes
    For some reason, changing the names of the motors magicly made it
    compile properly with no errors.
    
    coding is complete execpt for autonomous mode unless we want to change
    button mapping.

--- claw.c
+++ claw.c
@@ -1,15 +1,17 @@
 // Fill out later
-#define SL 0
-#define SR 0
-#define CF 0
-#define CL 0
-#define CR 0
+//#define SL 0
+//#define SR 0
+//#define CF 0
+//#define CL 0
+//#define CR 0
 // Speed difference to change height
 #define SDU 10
 // Scissor Base Speed
 #define SB 150
 // Speed to open/close claw
 #define CS 50
+// Speed to lift/lower claw
+#define CR 50
 bool ClawOpen = false;
 
 void clsetup()
@@ -20,30 +22,40 @@ void clsetup()
 // Scissor Up
 void srup()
 {
-	motor[SL] += SDU;
-	motor[SR] += SDU;
+	motor[SL] = SB + SDU;
+	motor[SR] = SB + SDU;
 }
 // Scissor Down
 void srdown()
 {
-	motor[SL] -= SDU;
-	motor[SR] -= SDU;
+	motor[SL] = SB - SDU;
+	motor[SR] = SB - SDU;
 }
 // Claw Open
 void clopen()
 {
-	motor[CF] = CS;
+	motor[CG] = CS;
 	sleep(500);
-	motor[CF] = 0;
+	motor[CG] = 0;
 	ClawOpen = true;
 }
 // Claw Close
 void clclose()
 {
-	motor[CF] = -CS;
+	motor[CG] = -CS;
 	sleep(500);
-	motor[CF] = 0;
+	motor[CG] = 0;
 	ClawOpen = false;
+}
+// Claw Up
+void clup()//                                 reverse one of the motors wires so they turn in different directions
+{
+motor[CR] = CR;
+}
+// Claw Down
+void cldown()//                               reverse one of the motors wires so they turn in different directions
+{
+motor[CR] = -CR;
 }
 // Claw Open or Close
 void clopcl()
