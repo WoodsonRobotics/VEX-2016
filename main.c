40eb51a7438188a77095627f2524383e49032961
wtwrobot <wtwoodsonrobotics@gmail.com>
2017-01-10 16:03:27 -0500

    main: Format control structures properly

--- main.c
+++ main.c
@@ -27,18 +27,23 @@ void auto()
 {
 	srsetup();
 	int temp = ((SensorValue(LeftOrRight)/4095.0)*3);//potentiometer values range from 0 to 4095 with leftmost being 0 and rightmost being 4095
-	if(temp==0){
+	if(temp==0)
+	{
 
-		}else if(temp==1){
+	}
+	else if(temp==1)
+	{
 
-		}else{
+	}
+	else
+	{
 
-		}
+	}
 	sleep(1000);
 }
 
 task autonomous()  { auto(); }
 task usercontrol() { remote(); }
 
-task main() { remote(); }
-//task main() { auto(); }
+//task main() { remote(); }
+task main() { auto(); }
