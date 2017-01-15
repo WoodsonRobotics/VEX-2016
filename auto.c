280fba06f98f1167e300d1ea0f067e8c55554a77
Jacob Adams <tookmund@gmail.com>
2017-01-15 12:16:12 -0500

    auto: Rewrite auto code

--- auto.c
+++ auto.c
@@ -20,61 +20,18 @@ void drive (int FB, int LR, int RO)
 void auto()
 {
 	srsetup();
-	int temp = ((SensorValue(LeftOrRight)/4095.0)*3);//potentiometer values range from 0 to 4095 with leftmost being 0 and rightmost being 4095
-	if(temp == 0)
-	{
-		// Move forward and slightly left
-		drive(127,-50,0);
-		clopen();
-		sleep(1000);
-		clstop();
-		// Fully extend scissor
-		srup();
-		sleep(5000);
-		// Move forward
-		drive(127,0,0);
-		srhold();
-		sleep(750);
-		// Stop driving
-		drive(0,0,0);
-		drive(0,32,0);
-		while(1)
-		{
-			clup();
-			sleep(250);
-			cldown();
-			sleep(250);
-		}
-	}
-	else if(temp == 2)
-	{
-		// Move forward and slightly left
-		drive(127,50,0);
-		clopen();
-		sleep(500);
-		clstop();
-		// Fully extend scissor
-		srup();
-		sleep(5000);
-		// Move forward
-		drive(127,0,0);
-		srhold();
-		sleep(750);
-		// Stop driving
-		drive(0,0,0);
-		drive(0,-32,0);
-		while(1)
-		{
-			clup();
-			sleep(250);
-			cldown();
-			sleep(250);
-		}
-	}
-	else if (temp == 1)
-	{
-		sleep(15000);
-	}
+	//int temp = ((SensorValue(LeftOrRight)/4095.0)*3);//potentiometer values range from 0 to 4095 with leftmost being 0 and rightmost being 4095
+
+	// Fully Extend Scissor
+	srup();
+	sleep(5000);
+	srhold();
+	// Drive forward slowly
+	drive(64,0,0);
+	sleep(500)
+	drive(0,0,0);
+
+
 }
 
 // Motors running at speeds less than 20 make a whining sound that is not great for the motor.
