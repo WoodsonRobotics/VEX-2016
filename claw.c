e868e2911f0b59ebea134bf5b3f765d997e3ecba
wtwrobot <wtwoodsonrobotics@gmail.com>
2017-02-14 15:21:54 -0500

    Revert "Raymond's ready for Hey-Day!"
    This reverts commit b43e24b75470294f365d15069272ee48c5116d2a.
    
    # Conflicts:
    #	claw.c
    #	main.c
    #	remote.c

--- claw.c
+++ claw.c
@@ -10,6 +10,11 @@
 void srsetup()
 {
 	slaveMotor(ScissorSlave,Scissor);
+<<<<<<< HEAD
+=======
+  slaveMotor(RightClawSlave,RightClaw);
+  slaveMotor(LeftClawSlave,LeftClaw);
+>>>>>>> parent of b43e24b... Raymond's ready for Hey-Day!
 
 
 	motor[LeftClaw]=ClSpd;
@@ -21,11 +26,16 @@ void srsetup()
 // Scissor Hold
 void srhold(int power)
 {
+<<<<<<< HEAD
 
 
 	int motorBuffer = 50;
 	if((power < motorBuffer && power > -motorBuffer)||(nMotorEncoder[Scissor]<ScrLowLmt&&power<0)||(nMotorEncoder[Scissor]>ScrUpLmt&&power>0)) power = 0;
 
+=======
+	int motorBuffer = 50;
+	if((power < motorBuffer && power > -motorBuffer)||(SensorValue(ScissorDownS)&&power<0)/*||(SensorValue(ScissorUpS)&&power>0)*/) power = 0;
+>>>>>>> parent of b43e24b... Raymond's ready for Hey-Day!
 
 	motor[Scissor] = power;
 }
@@ -59,4 +69,14 @@ motor[RightClaw]=ClSpd;
 // Right Claw Out
 void RClO(){
 motor[RightClaw]=-ClSpd;
+<<<<<<< HEAD
+=======
+}
+
+void RCC(){
+	int S = SensorValue(RightClawRot);
+if(S<(inR-clB)) RClI();
+else if(S>(inR+clB)) RClO();
+else RClS();
+>>>>>>> parent of b43e24b... Raymond's ready for Hey-Day!
 }
--- remote.c
+++ remote.c
@@ -5,6 +5,7 @@
 // Scissor
 #define ScissorMove	vexRT[Ch3]
 // LeftClaw
+<<<<<<< HEAD
 #define LeftClawOut	vexRT[Btn5D]
 #define LeftClawIn  vexRT[Btn5U]
 // RightClaw
@@ -12,6 +13,16 @@
 #define RightClawIn  vexRT[Btn6D]
 // shortcut quick-key for closing the claw
 #define ClawClose vexRt[Btn7U]
+=======
+#define LeftClawOut	vexRT[Btn5U]
+#define LeftClawIn  vexRT[Btn5D]
+// RightClaw
+#define RightClawOut vexRT[Btn6U]
+#define RightClawIn  vexRT[Btn6D]
+// CloseClaw
+#define clawCloseToggle vexRT[Btn7U]
+#define clawOpen vexRT[Btn7D]
+>>>>>>> parent of b43e24b... Raymond's ready for Hey-Day!
 // Freak Out
 #define FreakOut vexRT[Btn8R]
 
@@ -38,9 +49,10 @@ void remote()
 
 		motor[LeftWheels] = Left;
 		motor[RightWheels] = Right;
-
+if(FreakOut) stopAllMotors();
 		// Scissor Control
 		srhold(ScissorMove);
+<<<<<<< HEAD
 		// claw shortcut btns
 		if(clawClose)
 
@@ -55,5 +67,34 @@ void remote()
 		else RClS();
 
 		if(FreakOut) stopAllMotors();
+=======
+		if(FreakOut) stopAllMotors();
+		if(clawCloseToggle){
+			if(clawCloseBtnChng){
+				clawClose=!clawClose;
+				clawCloseBtnChng=false;
+			}
+			}else{
+			clawCloseBtnChng=true;
+		}
+
+
+
+if(FreakOut) stopAllMotors();
+		// Left Claw Control
+		if(LeftClawOut || clawOpen) LClO();
+		else if(LeftClawIn)  LClI();
+		else if(clawClose) LCC();
+		else LClS();
+		if(FreakOut) stopAllMotors();
+		// Right Claw Control
+		if(RightClawOut || clawOpen) RClO();
+		else if(RightClawIn)  RClI();
+		else if(clawClose) RCC();
+		else RClS();
+
+		if(FreakOut) stopAllMotors();
+		sleep(1);
+>>>>>>> parent of b43e24b... Raymond's ready for Hey-Day!
 	}
 }
